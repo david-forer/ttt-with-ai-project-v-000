1ae87081141401600c195a12bbebbb487146d411
David Forer <david.j.forer@gmail.com>
2019-06-10 13:45:01 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -27,7 +27,7 @@ class Game
     WIN_COMBINATIONS.detect do |winner|
       @board.cells[winner[0]] == @board.cells[winner[1]] && 
       @board.cells[winner[1]] == @board.cells[winner[2]] &&
-      (@board.cells[winner[0]] == "X" || @board.cells[winner[0]] == "Y"
+      (@board.cells[winner[0]] == "X" || @board.cells[winner[0]] == "O")
     end 
   end
   
